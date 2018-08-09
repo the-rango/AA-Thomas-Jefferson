64177a9568124d7eb6388e73b80746490442587d
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-08-09 14:19:14 -0700

    Fixing indent

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ CORS(app)
 
 @app.route("/", methods=['POST'])
 def helloWorld():
-    quater = request.form['quarter']
-    year = request.form['year']
-    code = request.form['code']
-    
+	quater = request.form['quarter']
+	year = request.form['year']
+	code = request.form['code']
+
 	bu = os.environ.get('PORTOFCALL')
-    
+
 	try:
 		with urllib.request.urlopen(bu.format(quarter, year, code)) as inf:
-            src = inf.read().decode('utf-8')
+			src = inf.read().decode('utf-8')
 	except:
 		chart = pygal.Line(no_data_text='Course Not Found',
-			   style=DefaultStyle(no_data_font_size=40))
+		style=DefaultStyle(no_data_font_size=40))
 		chart.add('line', [])
 		src = chart.render_data_uri()
-	
-    if client_agent.browser.strip() == 'msie' or 'Edge' in client_agent.string:
-    	return '<img src={} />'.format(src)
-    else:
-        return '<embed type="image/svg+xml" src= {} />'.format(src)
-  
+
+	if client_agent.browser.strip() == 'msie' or 'Edge' in client_agent.string:
+		return '<img src={} />'.format(src)
+	else:
+		return '<embed type="image/svg+xml" src= {} />'.format(src)
+
