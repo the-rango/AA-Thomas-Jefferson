3c4723dbf49bec439129b50b69221e8bce89a22c
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-08-09 14:41:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pygal
 from pygal.style import DefaultStyle
 import bcrypt
 import os
+import urllib.request, html, urllib.parse
+from urllib.parse import urlparse
+
 
 app = Flask(__name__)
 CORS(app)
@@ -21,7 +24,8 @@ def main(quarter, year, code):
 		style=DefaultStyle(no_data_font_size=40))
 		chart.add('line', [])
 		src = chart.render_data_uri()
-
+	
+	client_agent = request.user_agent
 	if client_agent.browser.strip() == 'msie' or 'Edge' in client_agent.string:
 		return '<img src={} />'.format(src)
 	else:
